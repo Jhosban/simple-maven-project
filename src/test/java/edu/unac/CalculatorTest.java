4d4198fd0c2f1cb34722b64a3597b894076123b3
Jhosban Barajas <jhosban8@gmail.com>
2025-06-03 19:33:46 -0500

    Add subtract test for CalculatorTest

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -17,7 +17,7 @@ class CalculatorTest {
         assertEquals(6, calc.multiply(2, 3));
     }
 
-   /* @Test
+    @Test
     void testSubtraction() {
         Calculator calc = new Calculator();
         assertEquals(2, calc.subtract(5, 3));
@@ -25,20 +25,20 @@ class CalculatorTest {
         assertEquals(0, calc.subtract(7, 7));
     }
 
-    @Test
-    void testDivision() {
-        Calculator calc = new Calculator();
-        assertEquals(4, calc.divide(8, 2));
-        assertEquals(3, calc.divide(10, 3));
-        assertEquals(0, calc.divide(0, 5));
-    }
-
-    @Test
-    void testDivisionByZeroThrowsException() {
-        Calculator calc = new Calculator();
-        ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> {
-            calc.divide(10, 0);
-        });
-        assertEquals("Cannot divide by zero.", thrown.getMessage());
-    }*/
+//    @Test
+//    void testDivision() {
+//        Calculator calc = new Calculator();
+//        assertEquals(4, calc.divide(8, 2));
+//        assertEquals(3, calc.divide(10, 3));
+//        assertEquals(0, calc.divide(0, 5));
+//    }
+//
+//    @Test
+//    void testDivisionByZeroThrowsException() {
+//        Calculator calc = new Calculator();
+//        ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> {
+//            calc.divide(10, 0);
+//        });
+//        assertEquals("Cannot divide by zero.", thrown.getMessage());
+//    }
 }
